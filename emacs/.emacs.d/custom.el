a3af9c063170537840ca7073cc00da784008b9e4
Aaron Yao-Smith <aaronyaosmith@gmail.com>
2020-01-20 18:03:39 -0500

    Update

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -7,10 +7,12 @@
  '(custom-safe-themes
    (quote
     ("a5956ec25b719bf325e847864e16578c61d8af3e8a3d95f60f9040d02497e408" "420459d6eeb45aadf5db5fbcc3d6990b65141c104911f7359454fc29fa9d87a0" default)))
- '(org-agenda-files nil)
+ '(org-agenda-files
+   (quote
+    ("/home/aaron/org/agenda/CoinFund.org" "/home/aaron/org/agenda/Cornell.org" "/home/aaron/org/agenda/SWE.org" "/home/aaron/org/agenda/behavioral.org" "/home/aaron/org/agenda/reading_list.org")))
  '(package-selected-packages
    (quote
-    (auto-complete ess-smart-underscore py-autopep8 pyenv-mode elpy gruvbox-theme org-edna))))
+    (py-autopep8 pyenv-mode elpy gruvbox-theme org-edna))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
