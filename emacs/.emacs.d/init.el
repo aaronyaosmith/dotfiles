795767ceb890e445d26a43eb66098382be8f95cf
Aaron Yao-Smith <aaronyaosmith@gmail.com>
2020-01-29 13:29:11 -0500

    Misc changes (opam, caps/ctrl swap, etc); i3lock not implemented

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -22,6 +22,10 @@
 (when (file-exists-p custom-file)
   (load custom-file))
 
+;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
+(require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
+;; ## end of OPAM user-setup addition for emacs / base ## keep this line
+
 ;; Package-specific settings in orgfile
 (require 'org)
 (org-babel-load-file
@@ -35,3 +39,4 @@
 (let ((elapsed (float-time (time-subtract (current-time)
                                           emacs-start-time))))
   (message "Loading settings...done (%.3fs)" elapsed))
+
--- env/.bash_profile
+++ env/.bash_profile
@@ -4,3 +4,8 @@
 
 [[ -f ~/.bashrc ]] && . ~/.bashrc
 [[ -f ~/.profile ]] && . ~/.profile
+export PATH="$HOME/.jenv/bin:$PATH"
+eval "$(jenv init -)"
+
+# opam configuration
+test -r /home/aaron/.opam/opam-init/init.sh && . /home/aaron/.opam/opam-init/init.sh > /dev/null 2> /dev/null || true
