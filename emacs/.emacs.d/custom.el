4c3bc5e399abc172f0f9bddcd594237abda12fd4
Aaron Yao-Smith <aaronyaosmith@gmail.com>
2020-02-29 22:59:43 -0500

    add use-package to emacs

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -8,7 +8,7 @@
     ("a22f40b63f9bc0a69ebc8ba4fbc6b452a4e3f84b80590ba0a92b4ff599e53ad0" "a5956ec25b719bf325e847864e16578c61d8af3e8a3d95f60f9040d02497e408" "420459d6eeb45aadf5db5fbcc3d6990b65141c104911f7359454fc29fa9d87a0" default)))
  '(package-selected-packages
    (quote
-    (paredit markdown-mode bison-mode gruvbox-theme magit whitespace-cleanup-mode interleave org-ref org-journal-list org-journal hideshow-org))))
+    (use-package paredit markdown-mode bison-mode gruvbox-theme magit whitespace-cleanup-mode interleave org-ref org-journal-list org-journal hideshow-org))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
